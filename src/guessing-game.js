b0539d073cbdd3fad55f195279b7420e4201f52f
Artur Abdullin <artur.abdullin@gmail.com>
2022-07-01 15:53:27 +0300

    feat: complete GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.left = 0;
+    this.right = 0;
+    this.mid = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.left = min;
+    this.right = max;
+  }
 
-    }
+  guess() {
+    return (this.mid = Math.ceil((this.right + this.left) / 2));
+  }
 
-    guess() {
+  lower() {
+    this.right = this.mid;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.left = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
